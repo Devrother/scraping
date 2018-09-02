15e0acd0e1d0d700d5533dddf3174a8fc02b4971
SeungUkLee <lsy931106@gmail.com>
2018-09-02 23:50:10 +0900

    add method and parameter description

--- run.py
+++ run.py
@@ -5,7 +5,9 @@ import requests
 def job_ids_scrapping(url):
     """
     scrap dev job id.
-    :return: id_list
+    :param url: url to scrap
+    :return: job_id_list:
+    :rtype list
     """
     params = dict(
         status='active',
@@ -18,7 +20,6 @@ def job_ids_scrapping(url):
 
     for i in range(10):
         resp = requests.get(url=url + search_api, params=params).json()
-
         for job_data in resp['data']['jobs']['data']:
             job_id_list.append(job_data['id'])
 
@@ -32,6 +33,11 @@ def job_ids_scrapping(url):
 
 
 def job_data_scrapping(job_id_list, url):
+    """
+    Scrap job data using job id.
+    :param job_id_list:
+    :param url: url to scrap
+    """
     jobs_api = '/api/v1/jobs/'
     job_data_list = []
 
