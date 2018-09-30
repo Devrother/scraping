8b245f0622bca1b24c5610df2ab888192226695e
SeungukLee <lsy931106@gmail.com>
2018-09-30 21:27:24 +0900

    Edit asyncio.gather() to asyncio.as_completed() and asyncio.Semaphore value

--- serverless-scrap/scrap/wanted_scrap.py
+++ serverless-scrap/scrap/wanted_scrap.py
@@ -1,111 +1,102 @@
 import asyncio
 import json
+
 from aiohttp import ClientSession
-import urllib.request
+
+# 1. dynamoDB 에서 job_id를 들고온다.
+# 2. total 값을 구하고 이 값을 이용하여 job id 를 scrap 한다.
+# 3. DB 에서 들고온 id 랑 비교하여 scrap 되지 않은 job_id 를 구한다.
+# 4. 새롭게 scrap 한 job_id 를 저장한다.
+# 5. scrap 되지않은 job_data 구한다.
+# 6. scrap 한 데이터를 파싱한다.
+# 7. company data 와 job data 로 구분한다.
+# 8. 이미지 url 은 sqs 에 전달.
+# 9. 파싱한 job_data 를 저장한다.
 
 
-############
-# 1. total 값을 구한다.  get_total_value
-# 2. elasticache 에서 저장된 id를 들고온다. get_job_id_elasticache
-############
-# 3. job id 를 구하기 위해 total 값까지 반복문을 통해 요청할 task 를 생성하여 job id 를 구한다. fetch_job_id
-############
-# 4. 구한 job id 와 elasticache 에 저장되어있던 id 와 비교하여 없는 id 를 구별한다. compare_id
-############
-# 5. 그 id 를 통해 job data 를 구한다. bound_fetch_job_data, fetch_job_data
-############
-# 6. 구해진 job data 는 aws rds 에 저장한다. insert_job_data_rds
-# 7. 새로운 job id 는 elasticache 에 저장한다  insert_job_id_elasticache
-############
+GET_JOB_ID_URL = 'http://www.wanted.co.kr/api/v3/search?' \
+                 'status=active&limit=12&tag_type_id=518&job_sort=-confirm_time' \
+                 '&offset={0}'
+GET_JOB_DATA_URL = 'https://www.wanted.co.kr/api/v1/jobs/{0}?lang=ko'
+LIMIT = 12
+SEMA = asyncio.Semaphore(10)
+STRINGS_TO_PARSE_COMPANY_DATA = ['company_id', 'company_name', 'company_info', 'location', 'logo_thumb_img']
+STRINGS_TO_PARSE_JOB_DATA = ['id', 'company_name', 'position', 'jd', 'create_time', 'company_id']
 
 
-def get_job_id_elasticache():
-    pass
+def main(event, context):
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(scrap_init(loop))
 
 
-def compare_id():
-    pass
+async def scrap_init(loop):
+    async with ClientSession(loop=loop) as session:
+        # Get total value
+        total = await get_total_value(session, GET_JOB_ID_URL.format(0))
 
-
-def insert_job_data_rds():
-    pass
-
-
-def insert_job_id_elasticache():
-    pass
-
-
-def get_total_value(url: str) -> int:
-    print("start get_total_value", url)
-    res_body = json.loads(urllib.request.urlopen(url).read().decode("utf-8"))
-    return res_body['data']['jobs']['total']
-
-
-async def fetch_job_data(url: str, session: ClientSession) -> int:
-    async with session.get(url) as response:
-        # 가끔씩 text/html 로 응답이 와서 ContentTypeError 가 발생. content_type 에 None 대입.
-        res_body = await response.json(content_type=None)
-        print("end get_job_data", url)
-        return res_body['id']
-
-
-async def bound_fetch_job_data(sem: asyncio.Semaphore, url: str, session: ClientSession) -> int:
-    # Getter function with semaphore.
-    async with sem:
-        return await fetch_job_data(url, session)
-
-
-async def fetch_job_id(url: str) -> list:
-    print("get_job_id start : ", url)
-    async with ClientSession() as session:
-        async with session.get(url) as response:
-            res_job_list = (await response.json())['data']['jobs']['data']
-            job_id_list = [data['id'] for data in res_job_list]
-            print("get_job_id end : ", url)
-            return job_id_list
-
-
-async def scrap_job_ids() -> list:
-    limit = 12
-    url = 'http://www.wanted.co.kr/api/v3/search?' \
-          'status=active&limit=12&tag_type_id=518&job_sort=-confirm_time' \
-          '&offset={0}'
-    total = get_total_value(url.format(0))
-    print("total : ", total)
-    tasks = [
-        asyncio.ensure_future(fetch_job_id(url.format(offset)))
-        for offset in range(0, total, limit)
-    ]
-    result = await asyncio.gather(*tasks)
-    return [y for x in result for y in x]  # flatten list
-
-
-async def scrap_job_datas(job_id_list: list):
-    url = "https://www.wanted.co.kr/api/v1/jobs/{0}?lang=ko"
-    sem = asyncio.Semaphore(800)
-
-    async with ClientSession() as session:
+        # Get job ids using total value
         tasks = [
-            asyncio.ensure_future(bound_fetch_job_data(sem, url.format(job_id), session))
-            for job_id in job_id_list
+            asyncio.ensure_future(get_job_id(session, GET_JOB_ID_URL.format(offset)))
+            for offset in range(0, total, LIMIT)
         ]
         result = await asyncio.gather(*tasks)
-        print("scrap_job_data : ", result)
-        print("job_data_len", result.__len__())
+        job_ids = [y for x in result for y in x]  # flatten list
+
+        # Get job datas using job ids
+        fs = [get_job_data(session, GET_JOB_DATA_URL.format(job_id), SEMA) for job_id in job_ids]
+        for future in asyncio.as_completed(fs, loop=loop):
+            data = await future
+            (parsed_company_data, parsed_job_data) = parse_data(data)
+            # await save_company_data
+            # await save_job_data
+            print("######")
+            print("com_data: ", json.dumps(parsed_company_data, ensure_ascii=False))
+            print("job_data: ", json.dumps(parsed_job_data, ensure_ascii=False))
+
+        # tasks2 = [
+        #     asyncio.ensure_future(get_job_data(session, GET_JOB_DATA_URL.format(job_id), SEM))
+        #     for job_id in job_ids
+        # ]
+        # (done, pending) = await asyncio.wait(tasks2, timeout=250)
+        # if pending:
+        #     print("there is {} tasks not completed".format(len(pending)))
+        # for f in pending:
+        #     for fr in f.get_stack():
+        #         print(fr.f_locals)
+        #     f.cancel()
+        # for f in done:
+        #     x = await f
+        #     print("job data :", x)
 
 
-def init_scrap(event, context):
-    print("create loop")
-    loop = asyncio.get_event_loop()
+async def get_job_data(session, url, sem):
+    async with sem:
+        # print("get_job_data start : ", url)
+        async with session.get(url) as response:
+            return await response.json()
 
-    print("start scrap_job_ids routine")
-    job_id_list = loop.run_until_complete(scrap_job_ids())
-    print("job_id_list len", job_id_list.__len__())
-    print("end scrap_job_ids routine")
 
-    print("start scrap_job_datas_routine")
-    loop.run_until_complete(scrap_job_datas(job_id_list))
-    print("end scrap_job_data_routine")
+def parse_data(data):
+    parsed_job_data = {}
+    parsed_company_data = {}
 
-    loop.close()
-    print("close loop")
+    for s in STRINGS_TO_PARSE_JOB_DATA:
+        parsed_job_data[s] = data[s]
+
+    for s in STRINGS_TO_PARSE_COMPANY_DATA:
+        parsed_company_data[s] = data[s]
+
+    return parsed_company_data, parsed_job_data
+
+
+async def get_total_value(session, url):
+    async with session.get(url) as response:
+        return (await response.json())['data']['jobs']['total']
+
+
+async def get_job_id(session, url):
+    # print("get_job_id start : ", url)
+    async with session.get(url) as response:
+        res_job_list = (await response.json())['data']['jobs']['data']
+        # print("get_job_id end : ", url)
+        return [data['id'] for data in res_job_list]
